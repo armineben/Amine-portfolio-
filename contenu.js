037986306a888ae7477b89d386400e22d6d42e52
Amine BENALI <aminebenali3@gmail.com>
2026-04-28 10:35:04 +0100

    Update contenu.js

--- contenu.js
+++ contenu.js
@@ -11,38 +11,10 @@ const PORTFOLIO_CONTENU = {
 
   // 📁 images/logos/
   logos: [
-    // "logo-salle-sport.png",
-    // "logo-patisserie.png",
-  ],
-
-  // 📁 images/affiches/
-  affiches: [
-    // "affiche-enactus-1.jpg",
-    // "evenement-master2.jpg",
-  ],
-
-  // 📁 images/cv/
-  cv: [
-    // "cv-amine.pdf",
-    // "cv-amine-apercu.jpg",
-  ],
-
-  // 📁 images/projets/
-  projets: [
-    // "fondation-visuel-1.jpg",
-    // "bpgo-support-1.jpg",
-  ],
-
-  // 📁 images/excel/
-  excel: [
-    // "dashboard-kpi-1.jpg",
-    // "tableau-vba.jpg",
-  ],
-
-  // 📁 images/publications/
-  publications: [
-    // "post-instagram-1.jpg",
-    // "post-linkedin-1.jpg",
+    "logo-couverture-patisserie.png",
+    "logo-couverture-salle-sport.png",
+    "logo-patisserie.jpg",
+    "logo-salle de sport.jpg",
   ],
 
 };
