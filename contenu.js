cf3e0321cdc9b79a04b61ef4103dfb30de0528bb
Amine BENALI <aminebenali3@gmail.com>
2026-04-29 09:13:25 +0100

    Update contenu.js

--- contenu.js
+++ contenu.js
@@ -27,7 +27,9 @@ const PORTFOLIO_CONTENU = {
   // HTML à la racine du dépôt
   projets: [
     "projet-fondation.html",
-    "projet-bpgo.html",
+    "projet-bpgo.html"
+    "projet-innovons.html",
+    "projet-nouveau.html",
   ],
 
   // 📁 images/images/excel/
