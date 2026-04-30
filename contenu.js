e430757cfeded7199f5813e81823ca6b990a8f07
Amine BENALI <aminebenali3@gmail.com>
2026-04-30 12:19:15 +0100

    Update contenu.js

--- contenu.js
+++ contenu.js
@@ -32,6 +32,7 @@ const PORTFOLIO_CONTENU = {
   // 📁 Dossier : images/images/excel/
   excel: [
     "projet-vba.html",
+    "GESTION-DE-LA-CLIENTÈLE.html",
     "Tableau-aide-à-la-décision.png",
     "bouton-importation-data.png",
     "Bouton-création-feuille-automatique.png", 
